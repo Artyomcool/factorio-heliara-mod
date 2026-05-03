d0a1e23538a7e86bcc8cbad223c77fa3ad209745
artem.drozdov <artyomcool2@gmail.com>
2026-05-03 17:19:30 +0200

    Big space platform hub

--- src/entity/big_space_platform_hub.lua
+++ src/entity/big_space_platform_hub.lua
@@ -18,6 +18,29 @@ local make_tile_area = function(area, name)
     return result
 end
 
+local function remove_shadows(t)
+  if type(t) ~= "table" then return end
+
+  for i = #t, 1, -1 do
+    local v = t[i]
+
+    if type(v) == "table" then
+      if v.draw_as_shadow then
+        table.remove(t, i)
+      else
+        remove_shadows(v)
+      end
+    end
+  end
+
+  for k, v in pairs(t) do
+    if type(k) ~= "number" and type(v) == "table" then
+      remove_shadows(v)
+    end
+  end
+end
+
+
 local entity = wrap_for_data(
     function ()
         local entity = table.deepcopy(data.raw["space-platform-hub"]["space-platform-hub"])
@@ -30,21 +53,35 @@ local entity = wrap_for_data(
         entity.inventory_size = entity.inventory_size * 4
         entity.circuit_wire_max_distance = entity.circuit_wire_max_distance * 2
         entity.platform_repair_speed_modifier = entity.platform_repair_speed_modifier * 5
+
+        local s = entity.graphics_set.picture[4].layers[2].shift
+        s[1] = s[1] - 1
+        s[2] = s[2] - 1
+
+        table.insert(
+            entity.graphics_set.picture[4].layers,
+            1,
+            {
+                filename = "__heliara__/graphics/entity/big_space_hub/modules.png",
+                    width = 1024,
+                    height = 1024,
+                    scale = 0.5,
+                    shift = {0.5,-0.25}
+            }
+        );
+
         entity.graphics_set.picture[1] = {
             render_layer = "lower-object",
             layers = {
                 {
-                    filename = "__heliara__/graphics/entity/big_space_hub/platform-hub.png",
+                    filename = "__heliara__/graphics/entity/big_space_hub/bg.png",
                     width = 1024,
                     height = 1024,
-                    shift = {0, -0.25},
-                    scale = 0.5
+                    scale = 0.5,
+                    shift = {0.5,-0.25}
                 },
             }
         }
-        local s = entity.graphics_set.picture[4].layers[2].shift
-        s[1] = s[1] - 1
-        s[2] = s[2] - 1
 
         return entity
     end,
@@ -57,16 +94,16 @@ return {
     {
         common = {
             name = _name,
-            icon = "__space-age__/graphics/icons/space-platform-starter-pack.png",    -- fixme
-            icon_size = 64,
+            icon = "__heliara__/graphics/icons/" .. _name ..  ".png",
+            icon_size = 128
         },
         entity = entity,
         raw = {
             {
                 type = "space-platform-starter-pack",
                 name = _name,
-                icon = "__space-age__/graphics/icons/space-platform-starter-pack.png",    -- fixme
-                icon_size = 64,
+                icon = "__heliara__/graphics/icons/" .. _name ..  ".png",
+                icon_size = 128,
                 subgroup = "space-rocket",
                 order = "b[space-platform-starter-pack-big]",
                 inventory_move_sound = item_sounds.mechanical_large_inventory_move,
